1a0a6cbe2c1c994c2beb07d03c9c83f5638a05a4
bertdelaspeed <bertdelaspeed@gmail.com>
2021-09-03 16:54:02 +0000

    homepage fixed in home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Homepage from "../components/Home/Homepage";
-import Layout from "../components/Layout/Layout";
 
 export default function home() {
-  return <HomePage />;
+  return <Homepage />;
 }
